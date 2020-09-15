9353554f5bf01c4645ebbdadcc996b72bb60c507
Jeffrey Enamorado <jenamorado@ymail.com>
2020-09-15 14:10:28 -0400

    ?

---
+++ MyFirstGame/Assets/cha.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cha : MonoBehaviour
+{
+    public KeyCode moveL;
+    public KeyCode moveR;
+
+    public float horizVel = 0;
+    public int laneNum = 2;
+    public string controlLocked = "n";
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(horizVel, 0, 4);
+
+        if ((Input.GetKeyDown(moveL)) && (laneNum > 1) && controlLocked == "n")
+        {
+            horizVel = -2;
+            StartCoroutine(stopSlide());
+            laneNum -= 1;
+
+        }
+        if ((Input.GetKeyDown(moveR)) && laneNum < 3)
+        {
+            horizVel = 2;
+            StartCoroutine(stopSlide());
+            laneNum += 1;
+
+        }
+    }
+    IEnumerator stopSlide()
+    {
+        yield return new WaitForSeconds (.3f);
+        horizVel = 0;
+    }
+}
