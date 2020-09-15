73d7462901277e12e00c757cfdf15a54de3e399b
Jeffrey Enamorado <jenamorado@ymail.com>
2020-09-15 15:58:29 -0400

    yerp

--- MyFirstGame/Assets/cha.cs
+++ MyFirstGame/Assets/cha.cs
@@ -17,28 +17,40 @@ public class cha : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         GetComponent<Rigidbody>().velocity = new Vector3(horizVel, 0, 4);
 
-        if ((Input.GetKeyDown(moveL)) && (laneNum > 1) && controlLocked == "n")
+        if ((Input.GetKeyDown(moveL)) && (laneNum > 1)&& (controlLocked == "n"))
         {
             horizVel = -2;
             StartCoroutine(stopSlide());
             laneNum -= 1;
+            controlLocked = "y";
 
         }
-        if ((Input.GetKeyDown(moveR)) && laneNum < 3)
+        if ((Input.GetKeyDown(moveR)) && (laneNum < 3)&& (controlLocked == "n"))
         {
             horizVel = 2;
             StartCoroutine(stopSlide());
             laneNum += 1;
+            controlLocked = "y";
 
         }
     }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "lethal")
+        {
+            Destroy (gameObject);
+        }
+    }
+
+
     IEnumerator stopSlide()
     {
-        yield return new WaitForSeconds (.3f);
+        yield return new WaitForSeconds (.5f);
         horizVel = 0;
+        controlLocked = "n";
     }
 }
